a7141126bd7b1f233f5c96fab5714cf1ceadf2f8
jacobmele-collab <jacob.mele@student.moringaschool.com>
2025-11-04 14:49:15 +0300

    new

--- NumberGuessingGame/obj/Debug/net8.0/NumberGuessingGame.AssemblyInfo.cs
+++ NumberGuessingGame/obj/Debug/net8.0/NumberGuessingGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberGuessingGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c7c5be972eb4094b3a70ee1d1a4f40159c52ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b75ff53258d5f0130cec9dce3454bb6844612354")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberGuessingGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberGuessingGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net9.0/c-number-guessing-game.AssemblyInfo.cs
+++ obj/Debug/net9.0/c-number-guessing-game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c-number-guessing-game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c9f110a20ca3d4e230add51be62276bbb5ecfb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b75ff53258d5f0130cec9dce3454bb6844612354")]
 [assembly: System.Reflection.AssemblyProductAttribute("c-number-guessing-game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c-number-guessing-game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
